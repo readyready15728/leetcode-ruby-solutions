249b4183ba765e500ed35791fd0dfa6dfb7c2641
Lynn Bradshaw <readyready15728@gmail.com>
2020-09-09 06:04:16 -0400

    Reworking valid-parentheses.rb

--- valid-parentheses.rb
+++ valid-parentheses.rb
@@ -1,29 +1,30 @@
 def is_valid(s)
     stack = []
-    
+
     s.chars.each do |c|
-        if c =~ /\(|{|\[/
-            stack.push(c)
-        elsif c == ')'
-            if stack[-1] == '('
-                stack.pop
-            else
-                return false
-            end
-        elsif c == '}'
-            if stack[-1] == '{'
-                stack.pop
-            else
-                return false
-            end
-        elsif c == ']'
-            if stack[-1] == '['
-                stack.pop
-            else
-                return false
-            end
+        case c            
+            when /\(|{|\[/
+                stack.push(c)
+            when ')'
+                if stack[-1] == '('
+                    stack.pop
+                else
+                    return false
+                end
+            when '}'
+                if stack[-1] == '{'
+                    stack.pop
+                else
+                    return false
+                end
+            when ']'
+                if stack[-1] == '['
+                    stack.pop
+                else
+                    return false
+                end
         end
     end
-            
+
     return stack.length == 0
 end
