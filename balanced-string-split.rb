147178d47a5d3e50b5813cac5d96cfb301adbd1d
Lynn Bradshaw <readyready15728@gmail.com>
2020-09-11 07:49:27 -0400

    Adding balanced-string-split.rb

---
+++ balanced-string-split.rb
@@ -0,0 +1,19 @@
+def balanced_string_split(s)
+    count_l = 0
+    count_r = 0
+    count = 0
+    
+    s.split('').each do |c|
+        if c == 'L'
+            count_l += 1
+        else
+            count_r += 1
+        end
+        
+        if count_l == count_r
+            count += 1
+        end
+    end
+    
+    count
+end
