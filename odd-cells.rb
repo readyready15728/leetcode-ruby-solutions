28aec9c05452baa92c1545ae4dedc79eedcd6da3
Lynn Bradshaw <readyready15728@gmail.com>
2020-09-12 05:59:55 -0400

    Adding odd-cells.rb

---
+++ odd-cells.rb
@@ -0,0 +1,21 @@
+def odd_cells(n, m, indices)
+    matrix = []
+    
+    n.times do
+        matrix.push [0] * m
+    end
+    
+    indices.each do |pair|
+        row, column = pair
+        
+        (0...m).each do |i|
+            matrix[row][i] += 1
+        end
+        
+        (0...n).each do |j|
+            matrix[j][column] += 1
+        end
+    end
+    
+    matrix.map { |row| row.count { |x| x.odd? } }.sum
+end
